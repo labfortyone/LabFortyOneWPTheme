00e5e71bbe35e1a4d97171000e0c86bc96957953
labfortyone <labfortyone@gmail.com>
2014-03-20 16:59:52 +0000

    Still developing the footer.

--- footer.php
+++ footer.php
@@ -12,11 +12,8 @@
                 <div class="grid_3 feed">
                     <div></div>
                 </div>
-                <div class="grid_1">
-                    s
-                </div>
-                <div class="grid_8 contact">
-                    <div class="grid_2 contact-info">
+                <div class="grid_9 contact">
+                    <div class="grid_3 contact-info">
                         <strong>Address</strong>
                         <p>Unit 4i Crossley Park</p>
                         <p>Crossley Road</p>
@@ -30,13 +27,12 @@
                         <strong>Email:</strong>
                         <p>labfortyone@gmail.com</p>
                     </div>
-                    <div class="grid_1">
+                    <div class="grid_1 icons">
                         <img alt="pin" src="wp-content/themes/LabFortyOneNew/images/address_pin.png"><br>
-                        <img alt="phone" src="wp-content/themes/LabFortyOneNew/images/phone.png"><br>
-                        <img alt="mail" src="wp-content/themes/LabFortyOneNew/images/mailletter.png">
+                        <img alt="phone" class="phone" src="wp-content/themes/LabFortyOneNew/images/phone.png"><br>
+                        <img alt="mail" class="mail" src="wp-content/themes/LabFortyOneNew/images/mailletter.png">
                     </div>
                     <div class="grid_5 contact-medias">
-                        <div class="grid_2">f</div>
                         <div class="grid_3 medias">
                             <img alt="pintrest" src="wp-content/themes/LabFortyOneNew/images/pintresticon.png">
                             <img alt="twitter" src="wp-content/themes/LabFortyOneNew/images/twitter_white.png">
